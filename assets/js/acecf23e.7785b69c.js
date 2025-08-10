a35b883b6514962367404021a9baf7698d446511
py-bay <py-bay@users.noreply.github.com>
2025-08-10 14:38:00 +0000

    deploy: dae858be49895d1f530eda5bd2809194c5912fdf

---
+++ assets/js/acecf23e.7785b69c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfacettenschach_landing=self.webpackChunkfacettenschach_landing||[]).push([[1903],{1912:a=>{a.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
